23cf3c3e94310c1717500b79e767b81568c3e103
Yuragranovsky <yuragranovsky@gmail.com>
2024-10-16 15:35:11 +0300

    Initial Code & application structure

---
+++ src/main/java/telran/employees/CompanyItems.java
@@ -0,0 +1,12 @@
+package telran.employees;
+
+import telran.view.*;
+
+public class CompanyItems {
+  //TODO
+  public static Item[] getItems(Company company) {
+    //TODO
+    //item for each serverce method
+     throw new UnsupportedOperationException();
+  }
+}
